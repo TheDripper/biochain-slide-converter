7d15daefd53a9d8e48ea1d16a5a0d3d03e5d2289
Dead Customer <deadcustomer@Deads-MacBook-Air.local>
2021-05-29 10:04:31 -0700

    ssh working, needs key

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-git clone https://github.com/TheDripper/biochain-slide-converter.git
+apk update
+apk add git
+apk add openssh
+git clone git@github.com:TheDripper/biochain-slide-converter.git
