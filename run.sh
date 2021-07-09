3e6c0ffafe915f8f680d28db41410b77f083c66f
TheDripper <deadcustomer@Deads-MacBook-Air.local>
2021-07-08 22:23:19 -0700

    sharp libvips error, compose/npm working

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-# npm install --loglevel verbose
-# npm run build
+apt-get update
+apt-get install curl -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+nvm install 14
+nvm alias default 14
+
+# confirm installation
+RUN node -v
+RUN npm -v
+npm install --loglevel verbose
+npm run build
 npm start --loglevel verbose
