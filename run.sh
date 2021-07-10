6c4f9902ceafb9e9554e7efd499b262ce5ae2258
TheDripper <deadcustomer@Deads-MacBook-Air.local>
2021-07-10 16:52:02 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 apt-get update
 apt-get install curl -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+PROFILE=~/.bash_profile
+echo '# NVM Setting' >> ${PROFILE}
+echo 'export NVM_DIR="$HOME/.nvm"' >> ${PROFILE}
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" ' >> ${PROFILE}
+source ${PROFILE}
 nvm install node
 nvm install 12
 nvm alias default 12
