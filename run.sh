a45384647aa3d7945655a5239d2e241c69d04fcd
TheDripper <deadcustomer@Deads-MacBook-Air.local>
2021-07-22 11:58:08 -0700

    stuff

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 apt-get update
 apt-get install curl -y
+apt-get install build-essential
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
-nvm install 12
-nvm alias default 12
+nvm install 14
+nvm alias default 14
 npm install -g nuxt
 npm install --loglevel verbose
 npm run dev
